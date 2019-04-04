871263532f04cb07802cabdf8b6a7e4a046f3e08
rexar <rexar1988@gmail.com>
2019-04-04 22:08:57 +0300

    add not lazy load module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './modules/about/about.component';
+
 const routes: Routes = [
+  {
+    path: 'about', component: AboutComponent
+  },
   {
     path: 'home', loadChildren: './modules/home/home.module#HomeModule'
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ExampleComponent } from './core/components/example/example.component';
 import { TestComponent } from './core/components/test/test.component';
 import { ModalFormComponent } from './modules/home/components/modal-form/modal-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AboutModule } from './modules/about/about.module';
 
 @NgModule({
   declarations: [
@@ -21,8 +22,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DialogModule,
     ReactiveFormsModule,
+    DialogModule,
+    AboutModule
   ],
   bootstrap: [AppComponent],
   entryComponents: [
